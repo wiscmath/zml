c2b5575b10df208a86ab2be44e01e60cec94e5a6
Nuwan Rajika Kumarasiri <nuwan.kumarasiri@wisc.edu>
2025-04-26 10:40:49 -0500

    add universal const

--- src/const.zig
+++ src/const.zig
@@ -7,13 +7,28 @@ pub const e = 2.7182818284590452353602874713526624977572470936999595749669676277
 // Archimedes's constant (π) - https://oeis.org/A000796
 pub const pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640623;
 
+// Phi or Golden ratio constant (Φ) = (1 + sqrt(5))/2 - https://oeis.org/A001622
+pub const phi = 1.6180339887498948482045868343656381177203091798057628621;
 
-test "display Eculer's number" {
+// sqrt(2) - https://oeis.org/A002193
+pub const sqrt2 = 1.41421356237309504880168872420969807856967187537694807317667;
+
+// sqrt(e) - https://oeis.org/A019774
+pub const sqrte = 1.6487212707001281468486507878141635716537761007101480115750;
+
+// sqrt(pi) - https://oeis.org/A002161
+pub const sqrtpi = 1.7724538509055160272981674833411451827975494561223871282138; 
+
+// sqrt(phi) - https://oeis.org/A139339
+pub const sqrtphi = 1.2720196495140689642524224617374914917156080418400; 
+
+test "display universal consts" {
     std.debug.print("{}\n", .{e});
-}
-
-test "display Archimede's constant" {
     std.debug.print("{}\n", .{pi});
+    std.debug.print("{}\n", .{phi});
+    std.debug.print("{}\n", .{sqrt2});
+    std.debug.print("{}\n", .{sqrte});
+    std.debug.print("{}\n", .{sqrtpi});
+    std.debug.print("{}\n", .{sqrtphi});
 }
-
 
