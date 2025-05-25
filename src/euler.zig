6e510f44611fd025238b10122a411b62a3f20334
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-05-25 18:20:43 -0500

    zml references

--- src/euler.zig
+++ src/euler.zig
@@ -3,12 +3,19 @@ const math = std.math;
 const testing = std.testing;
 const assert = std.debug.assert;
 
-pub fn euler(comptime T: type, n: T) T {
+pub fn euler1(comptime T: type, n: T) T {
     return math.pow(T, (1 + (1 / n)), n);
 }
 
+pub fn euler2(comptime T: type) T {
+    return math.exp(1);
+}
+
 test "test euler's number" {
-    const e = euler(u128, 10);
-    std.debug.print("{d}\n", .{e});
-    assert(euler(u128, 10) == 2.7891);
+    const e1 = euler1(u128, 10);
+    std.debug.print("{d}\n", .{e1});
+    assert(e1 == 2.7891);
+
+    const e2 = euler2(u128);
+    std.debug.print("{d}\n", .{e2});
 }
